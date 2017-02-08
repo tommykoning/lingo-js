ba0054d55f653ef8d0f208a425e53b60acc891e1
tommy koning <99039195@mydavinci.nl>
2017-02-08 14:36:42 +0100

    no message

--- lingo.js
+++ lingo.js
@@ -1,17 +1,27 @@
 var splinter = null
 
+//causes game to st
 function start() {
 	var nummer = Math.floor(Math.random() * words.length) ; 
 	var lingowoord = words[nummer];
 	splinter = lingowoord.split('');
-	document.getElementsByClassName('beginletter').innerHTML = 'splinter[0]';
+	for (var i = 0; i < 5;) {
+		document.getElementsByClassName('beginletter')[i].innerHTML = splinter[0];
+		i++;
+	}
+
 	console.log(splinter);
 }
 
+//resets game
 function reset() {
 	location.reload();
 }
 
+//checks answer 
 function check() {
+	var input = document.getElementById('answer_Input').value, 
+	answer_input = input.value;
 
-}
+	console.log(input);
+} 
